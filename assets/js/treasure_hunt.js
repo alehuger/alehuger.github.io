b846339e5bf87858eb77579c937d7bfb541bd486
alehuger <augustelehuger@hotmail.fr>
2021-08-24 01:16:09 +0200

    extra number check js

--- assets/js/treasure_hunt.js
+++ assets/js/treasure_hunt.js
@@ -1,11 +1,11 @@
 function check_square() {
-    var val = document.getElementById("check").value;
-    if (val == "4") {
-        console.log("sd")
+    var check4 = document.getElementById("check4").value;
+    var check9 = document.getElementById("check9").value; 
+    if ((check4 == "4") && (check9 == "9"))  {
+        console.log("checked ! ")
         $(".to_hide").css("visibility", "hidden");  
         $(".to_show").css("visibility", "visible");  
-        $(".birthday").css("background-size", "100%");
-        $("p").css("color", "green");       
+        $(".birthday").css("background-size", "100%");              
     }
 }
 
